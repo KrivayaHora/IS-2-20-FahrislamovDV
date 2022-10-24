3cd2246a79371bcf7eb978ff9211f59999b035e5
Данил Фахрисламов <gavnov952@mail.ru>
2022-10-24 20:45:04 +0500

    Добавлен "дизайн"

---
+++ Kursovaya_Rabota/Project/Forms/ManagementForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Kursovaya_Rabota
+{
+    partial class ManagementForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Employees = new System.Windows.Forms.Button();
+            this.RegComponent = new System.Windows.Forms.Button();
+            this.PcConf = new System.Windows.Forms.Button();
+            this.ManageClientBox = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel1.Controls.Add(this.Employees);
+            this.panel1.Controls.Add(this.RegComponent);
+            this.panel1.Controls.Add(this.PcConf);
+            this.panel1.Controls.Add(this.ManageClientBox);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(201, 503);
+            this.panel1.TabIndex = 6;
+            // 
+            // Employees
+            // 
+            this.Employees.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Employees.FlatAppearance.BorderSize = 0;
+            this.Employees.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Employees.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Employees.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Employees.Image = global::Kursovaya_Rabota.Properties.Resources.pngegg__4_;
+            this.Employees.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Employees.Location = new System.Drawing.Point(0, 221);
+            this.Employees.Name = "Employees";
+            this.Employees.Size = new System.Drawing.Size(201, 47);
+            this.Employees.TabIndex = 10;
+            this.Employees.Text = "Сотрудники";
+            this.Employees.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Employees.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Employees.UseVisualStyleBackColor = true;
+            this.Employees.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // RegComponent
+            // 
+            this.RegComponent.Dock = System.Windows.Forms.DockStyle.Top;
+            this.RegComponent.FlatAppearance.BorderSize = 0;
+            this.RegComponent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RegComponent.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RegComponent.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.RegComponent.Image = global::Kursovaya_Rabota.Properties.Resources.pngegg__6_;
+            this.RegComponent.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.RegComponent.Location = new System.Drawing.Point(0, 174);
+            this.RegComponent.Name = "RegComponent";
+            this.RegComponent.Size = new System.Drawing.Size(201, 47);
+            this.RegComponent.TabIndex = 9;
+            this.RegComponent.Text = "Учёт комплетующих";
+            this.RegComponent.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.RegComponent.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.RegComponent.UseVisualStyleBackColor = true;
+            this.RegComponent.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // PcConf
+            // 
+            this.PcConf.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PcConf.FlatAppearance.BorderSize = 0;
+            this.PcConf.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PcConf.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PcConf.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.PcConf.Image = global::Kursovaya_Rabota.Properties.Resources.pngegg__5_;
+            this.PcConf.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.PcConf.Location = new System.Drawing.Point(0, 127);
+            this.PcConf.Name = "PcConf";
+            this.PcConf.Size = new System.Drawing.Size(201, 47);
+            this.PcConf.TabIndex = 8;
+            this.PcConf.Text = "Конфигуратор";
+            this.PcConf.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.PcConf.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.PcConf.UseVisualStyleBackColor = true;
+            this.PcConf.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ManageClientBox
+            // 
+            this.ManageClientBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ManageClientBox.FlatAppearance.BorderSize = 0;
+            this.ManageClientBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ManageClientBox.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ManageClientBox.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ManageClientBox.Image = global::Kursovaya_Rabota.Properties.Resources.pngegg__7_;
+            this.ManageClientBox.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ManageClientBox.Location = new System.Drawing.Point(0, 80);
+            this.ManageClientBox.Name = "ManageClientBox";
+            this.ManageClientBox.Size = new System.Drawing.Size(201, 47);
+            this.ManageClientBox.TabIndex = 7;
+            this.ManageClientBox.Text = "Клиенты";
+            this.ManageClientBox.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ManageClientBox.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.ManageClientBox.UseVisualStyleBackColor = true;
+            this.ManageClientBox.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(201, 80);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(35, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PC Configurator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel3.Location = new System.Drawing.Point(200, 5);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(661, 80);
+            this.panel3.TabIndex = 7;
+            // 
+            // ManagementForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 507);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximumSize = new System.Drawing.Size(860, 507);
+            this.Name = "ManagementForm";
+            this.Padding = new System.Windows.Forms.Padding(23, 65, 23, 22);
+            this.TransparencyKey = System.Drawing.Color.SpringGreen;
+            this.Load += new System.EventHandler(this.ManagementForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button ManageClientBox;
+        private System.Windows.Forms.Button Employees;
+        private System.Windows.Forms.Button RegComponent;
+        private System.Windows.Forms.Button PcConf;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+    }
+}
--- Kursovaya_Rabota/WorkClasses.cs
+++ Kursovaya_Rabota/WorkClasses.cs
@@ -7,28 +7,9 @@ using MySql.Data.MySqlClient;
 
 namespace Kursovaya_Rabota
 {
-    static internal class ConnectionSQL   
+    static internal class Class
     {
-        static MySqlConnection ChucConnect = new MySqlConnection("server=10.90.12.110;port=33333;username=st_2_20_24;password=54843478;database=is_2_20_st24_KURS");
-        static MySqlConnection DConnect = new MySqlConnection("server = chuc.caseum.ru; port=33333;username=st_2_20_24;password=54843478;database=is_2_20_st24_KURS");
-        public static void OpenChuc()
-        {
-            ChucConnect.Open();
-        }
-        public static void CloseChuc()
-        {
-            ChucConnect.Close();
-        }
-
-        public static void  OpenD()
-        {
-            DConnect.Open();
-        }
-
-        public static void CloseD()
-        {
-            DConnect.Close();
-        }
+        
     }
 
 }
